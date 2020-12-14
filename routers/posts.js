77d08f06c2ebd227b8b190014d103734395688b8
musa_abdullah <musaabdalwahed@gmail.com>
2020-12-14 09:38:25 +0200

    set up the post route

--- routers/posts.js
+++ routers/posts.js
@@ -6,6 +6,20 @@ const router = Router();
 router.route('/')
   .get((req, res) => {
     res.json('Hello world');
+  })
+  .post((req, res) => {
+    res.json('post post');
+  });
+
+router.route('/:id')
+  .get((req, res) => {
+    res.json({ message: 'get one post', id: req.params.id });
+  })
+  .put((req, res) => {
+    res.json({ message: 'update one post', id: req.params.id });
+  })
+  .delete((req, res) => {
+    res.json({ message: 'delete one post', id: req.params.id });
   });
 
 module.exports = router;
